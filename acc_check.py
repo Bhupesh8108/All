6c3f343e4723c09c6776fd62dfe6f16ab580d988
Bhupesh Dawadi <98863362+Bhupesh8108@users.noreply.github.com>
2022-11-12 10:10:30 +0545

    Add files via upload

---
+++ acc_check.py
@@ -0,0 +1,23 @@
+import keras
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB, BernoulliNB ,MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+
+met = [DecisionTreeClassifier(),RandomForestClassifier(),LogisticRegression(),LinearRegression(),SVC(), GaussianNB(), BernoulliNB() ,MultinomialNB(), KNeighborsClassifier()]
+accu = []
+def accuracy(x,xt,y,yt):
+    for i in met:
+        try:
+            model = i
+            model.fit(x, y)
+            pred = model.predict(xt)
+            acc = accuracy_score(pred.astype(int), yt.astype(int))
+        except:
+            acc = 0
+        accu.append(acc)
+    print(met[accu.index(max(accu))],';',max(accu)])
+       
